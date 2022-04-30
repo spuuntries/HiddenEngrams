31af14aa53ba8a389e65d9a5c55d12f181003c56
spuun <69007078+spuuntries@users.noreply.github.com>
2022-04-30 22:16:41 +0700

    Update transformer.py
    J doesn't want to load... for some reason? Am I OOMing the colab? :thonk:

--- transformer.py
+++ transformer.py
@@ -1,8 +1,8 @@
 # Get the transformer model and tokenizer
 def get_transformer():
     from transformers import AutoTokenizer, AutoModelForCausalLM
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-    model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+    model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B")
     return model, tokenizer
 
 # Easy generation function
